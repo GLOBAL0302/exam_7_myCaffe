ff5d3f85f842a327c0b2a70cfcff0693966f02c9
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-06-01 15:05:09 +0400

    make a visual effects on menuItems

--- src/components/MenuItems/menuItem/menuItem.tsx
+++ src/components/MenuItems/menuItem/menuItem.tsx
@@ -10,7 +10,7 @@ const MenuItem:React.FC<Props> = ({menuItem, addMenuItem}) => {
   return (
     <button className="d-flex align-items-center p-3 border border-5 w-50" onClick={addMenuItem}>
       <img src={menuItem.title.img} alt="menuItemImg" style={{width:"100px"}}/>
-      <div>
+      <div className="ms-auto">
         <p>{menuItem.title.name}</p>
         <span>Price: {menuItem.title.price}</span>
       </div>
